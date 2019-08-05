d667fe19273c7566da7f16af4faade24710e2a8e
Jose Ruben de la Peña <hoth@hoth.local>
2019-08-05 11:12:31 -0500

    actualizacion Undefined index: periodo_escolar

--- matriculas/reporte.php
+++ matriculas/reporte.php
@@ -14,6 +14,9 @@
     $tabla2 = $_POST["tabla2"];
     $anio_reporte = substr($_POST["nombreBD"],10,12);
 
+    if ($_POST["periodo_escolar"] == '')
+        echo ("entra </br>");
+
     $swPerEsc      = $_POST["periodo_escolar"];
     
     print <<<EOF
