f06b41b0b92debd4d7b55142208802b7bdce80ed
Jose Ruben de la Peña <hoth@hoth.local>
2019-11-29 09:46:19 -0600

    reporte

--- residencias/bdselected.php
+++ residencias/bdselected.php
@@ -14,47 +14,81 @@
                 
             </div> <!-- end menu -->
             <table>
-                <td>
+                <td valign = "top">
                     <div id="sidebar">
                         <h3>&nbsp;&nbsp;Menu</h3>
                         <ul>
-                            <li><a href="selectBD.php">Base de Datos</a></li>
-                            <br>&nbsp
-                            <br>&nbsp
-                            <br>&nbsp
-                            <br>&nbsp
-                            <br>&nbsp
+                            <li><a href="selectBD.php">Base de Datos</a></li>                           
                         </ul>
                     </div> <!-- end sidebar -->
                 </td>
                 <td>
                     <div id="content">
-                        <p>Base de Datos Seleccionada.</p>
+                        </br>
+                        <p>Base de Datos Seleccionada: &nbsp
 EOF;
                         echo $_POST["nombreBD"];
                         print <<< EOF
-                        <br>&nbsp
-                        <br>&nbsp
-
+                     
+                        <table>  
                         <form method = "POST" action="reportes.php">
                             <fieldset>
-                                <legend>Selección de Reportes</legend>
-                                <div>
-                                    <input type="radio" name="reporte" value="reporte2">
+                                <tr> <legend>Selección de Reportes</legend> </tr> 
+                                    
+                                    </br>
+                                    <tr> <input type="radio" name="reporte" value="reporte2"> </tr>
                                     <label for="reporte2">Residencias por Carrera y Sector</label></br>
 
                                     <input type="hidden" name="nombreBD" value=
 EOF;
                                     echo $_POST["nombreBD"];
                                     print <<<EOF
-                                    >   
-                                    &nbsp</br>
-                                </div>
-                                <div>
-                                    <input id="submit" type="submit" name="submit" value="Aplicar">
-                                </div>
+                                    >
+                                    </br>
+                                <tr>
+                                    <td>
+                                    <table>
+                                        
+                                            <tr>
+                                                Sector:
+                                            <tr>
+                                            </br>
+                                            <tr>
+                                                <input type="checkbox" name="Sector_Privado" value="1">&nbsp; Privado
+                                            <tr>
+                                            </br>
+                                            <tr>
+                                                <input type="checkbox" name="Sector_Publico" value="1">&nbsp; Público
+                                            </tr>
+                                            </br>
+                                            <tr>
+                                                <input type="checkbox" name="Sector_EdPublico" value="1">&nbsp; Educ. Público
+                                            </tr>
+                                            </br>
+                                            <tr>
+                                                <input type="checkbox" name="Sector_EdPrivado" value="1">&nbsp; Educ. Privado
+                                            </tr>
+                                            </br>
+                                            <tr>
+                                                <input type="checkbox" name="Tipo_Todas" value="1">&nbsp; Todos
+                                            </tr>
+                                            </br>
+                                       
+                                    </table>
+                                    </td>  
+                                </tr>
+
+
+                                <tr>
+                                    <td> 
+                                        </br>
+                                        <input id="submit" type="submit" name="submit" value="Aplicar"> 
+                                    </td>
+                                </tr>
+
                             </fieldset>
                         </form>
+                        </table>
                      </div> <!-- end content -->
                 </td>
             </table>
